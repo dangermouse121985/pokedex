81741660d2cc0d5e911fa6c5b64329b3b5648aff
David <618375+dangermouse121985@users.noreply.github.com>
2023-08-09 14:36:49 -0400

    Added PokemonList Array with 3 Pokemon Objects

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let pokemonList = [];
+pokemonList = [
+    {
+        name: 'Bulbasaur',
+        type: ['grass', 'poison'],
+        species: 'Seed'
+    },
+    {
+        name: 'Charmander',
+        Type: 'Fire',
+        species: 'Lizard'
+    },
+    {
+        name: 'squirtle',
+        type: 'water',
+        species: 'Young Turtle'
+    }
+]
