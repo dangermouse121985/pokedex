f657be2fdc7642f75a1c01c503fe092ef8cd5f92
David <618375+dangermouse121985@users.noreply.github.com>
2023-08-10 12:32:28 -0400

    Added largbeasts class to each pokemon larger than 0.5

--- js/script.js
+++ js/script.js
@@ -4,30 +4,30 @@ pokemonList = [
         name: 'Bulbasaur',
         type: ['Grass', 'Poison'],
         species: 'Seed',
-        height: 0.7
+        height: 0.7,
     },
     {
         name: 'Charmander',
         Type: 'Fire',
         species: 'Lizard',
-        height: 0.6
+        height: 0.6,
     },
     {
         name: 'Squirtle',
         type: 'Water',
         species: 'Young Turtle',
-        height: 0.5
+        height: 0.5,
     }
 ]
 
 for (let i=0; i<pokemonList.length; i++) {
-    document.write(pokemonList[i].name + ' ');
-    document.write('(height - ' + pokemonList[i].height + ')');
-
     if (pokemonList[i].height > 0.5) {
-        document.write(' Wow that\'s big!!!<br>');
+        document.write(`<p class="largeBeasts">${pokemonList[i].name} `);
+        document.write(`(height - ${pokemonList[i].height})`);
+        document.write('Wow that\'s big!!!</p>');
     }
     else {
-        document.write('<br>')
+        document.write(`<p>${pokemonList[i].name} `);
+        document.write(`(height - ${pokemonList[i].height})</p>`);
     }
 }
