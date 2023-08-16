5a5be95f7df47d26afdeba2ad91a6167877f284d
David <dcrichlo.spsu@gmail.com>
2023-08-16 18:33:15 -0400

    wrapped pokemonList in an IIFE and updated js to reference pokemonRepository functions (getAll and add)

--- js/script.js
+++ js/script.js
@@ -1,37 +1,53 @@
-let pokemonList = [];
-pokemonList = [
-    {
-        name: 'Bulbasaur',
-        type: ['Grass', 'Poison'],
-        species: 'Seed',
-        height: 0.7,
-        largestPokemon: false
-    },
-    {
-        name: 'Charmander',
-        type: ['Fire'],
-        species: 'Lizard',
-        height: 0.6,
-        largestPokemon: false
-    },
-    {
-        name: 'Squirtle',
-        type: ['Water'],
-        species: 'Young Turtle',
-        height: 0.5,
-        largestPokemon: false
+let pokemonRepository = function (){
+    let pokemonList = [];
+    pokemonList = [
+        {
+            name: 'Bulbasaur',
+            type: ['Grass', 'Poison'],
+            species: 'Seed',
+            height: 0.7,
+            largestPokemon: false
+        },
+        {
+            name: 'Charmander',
+            type: ['Fire'],
+            species: 'Lizard',
+            height: 0.6,
+            largestPokemon: false
+        },
+        {
+            name: 'Squirtle',
+            type: ['Water'],
+            species: 'Young Turtle',
+            height: 0.5,
+            largestPokemon: false
+        }
+    ]
+
+    function getAll(){
+        return pokemonList;
     }
-]
+
+    function add(pokemon){
+        pokemonList.push(pokemon);
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    }
+}();
+
 
 //Height of the currently largest Pokemon
 let largestPokemonHeight = 0;
 //Counter used to identify the last Pokemon tagged as the largest
 let x = 0;
 //Iterate through the pokemonList to find the largest Pokemon
-pokemonList.forEach(function(pokemon, index){
+pokemonRepository.getAll().forEach(function(pokemon, index){
     if ( pokemon.height > largestPokemonHeight) {
         largestPokemonHeight = pokemon.height;
-        pokemonList[x].largestPokemon=false;
+        pokemonRepository.getAll()[x].largestPokemon=false;
         pokemon.largestPokemon = true;
         x=index;
     }
@@ -40,7 +56,7 @@ pokemonList.forEach(function(pokemon, index){
 /*Iterate through the pokemonList for loop to print each pokemon with it's height.
 Conditional loop will add a largeBeast class and a mesage if the Pokemon is larger than 0.5m*/
 function printArrayDetails(pokemonList) {
-    pokemonList.forEach(function(pokemon) {
+    pokemonRepository.getAll().forEach(function(pokemon) {
         document.write('<div class="grid__item">');
         if (pokemon.largestPokemon) {
             document.write(`<p class="largeBeasts">${pokemon.name} `);
@@ -55,4 +71,4 @@ function printArrayDetails(pokemonList) {
     })
 }
 
-printArrayDetails(pokemonList);
+printArrayDetails(pokemonRepository.getAll());
